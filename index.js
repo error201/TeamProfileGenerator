bf25d1650d85e3e56426cb3803af1ca208be6e1a
Jason A. Arter <jason.arter@gmail.com>
2023-01-26 19:22:51 -0800

    All functionality complete.

--- index.js
+++ index.js
@@ -1,23 +1,127 @@
+//  Globals.
 const inquirer = require("inquirer");
+const fs = require("fs");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
-const hasManager = false;
+const genHtml = require("./util/generateHtml");
+var hasManager = false;
+const team = [];
+
 
 // Start asking for user input.
-function main() {
+function prompts() {
     if (hasManager === false) {
-        inquirer
-            .prompt([
-                {
-                    type: 'input',
-                    message: "Enter the Team Manager's name",
-                    name: 'managerName',
-                },
-            ]).then((response) =>
-                response.confirm === response.password
-                    ? console.log('Success!')
-                    : console.log('You forgot your password already?!')
-            );
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: "Enter the Manager's name",
+                name: 'managerName',
+            },
+            {
+                type: 'input',
+                message: "Enter the Manager's employee ID.",
+                name: 'managerId',
+            },
+            {
+                type: 'input',
+                message: "Enter the Manager's email address.",
+                name: 'managerEmail',
+            },
+            {
+                type: 'input',
+                message: "Enter the Manager's office number.",
+                name: 'managerOfficeNumber',
+            },
+        ]).then((response) => {
+            hasManager = true;
+            const manager = new Manager(response.managerName,
+                response.managerId,
+                response.managerEmail,
+                response.managerOfficeNumber)
+            team.push(manager);
+            prompts();
+        });
+    } else {
+        inquirer.prompt([
+            {
+                type: 'list',
+                message: "Who would you like to add to the team?",
+                name: 'addMembers',
+                choices: ["Engineer", "Intern", "Finish", "Quit"],
+            },
+        ]).then((response) => {
+            if (response.addMembers == "Engineer") {
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        message: "Enter the Engineer's name",
+                        name: 'engineerName',
+                    },
+                    {
+                        type: 'input',
+                        message: "Enter the Engineer's employee ID.",
+                        name: 'engineerId',
+                    },
+                    {
+                        type: 'input',
+                        message: "Enter the Engineer's email address.",
+                        name: 'engineerEmail',
+                    },
+                    {
+                        type: 'input',
+                        message: "Enter the Engineer's GitHub username.",
+                        name: 'engineerGithub',
+                    },
+                ]).then((response) => {
+                    const engineer = new Engineer(response.engineerName,
+                        response.engineerId,
+                        response.engineerEmail,
+                        response.engineerGithub)
+                    team.push(engineer);
+                    prompts();
+                });
+            } else if (response.addMembers == "Intern") {
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        message: "Enter the Intern's name",
+                        name: 'internName',
+                    },
+                    {
+                        type: 'input',
+                        message: "Enter the Intern's employee ID.",
+                        name: 'internId',
+                    },
+                    {
+                        type: 'input',
+                        message: "Enter the Intern's email address.",
+                        name: 'internEmail',
+                    },
+                    {
+                        type: 'input',
+                        message: "Enter the Intern's school name.",
+                        name: 'internSchool',
+                    },
+                ]).then((response) => {
+                    const intern = new Intern(response.internName,
+                        response.internId,
+                        response.internEmail,
+                        response.internSchool)
+                    team.push(intern);
+                    prompts();
+                });
+            } else if (response.addMembers == "Finish") {
+                fs.writeFile("./dist/index.html", genHtml(team), function(err){
+                    if(err){
+                        console.log("Error writing file");
+                    }
+                })
+            } else {
+                return;
+            }
+        });
     }
 }
+
+prompts();
--- util/generateHtml.js
+++ util/generateHtml.js
@@ -4,8 +4,8 @@ const generateTeam = team => {
     // create the manager html
     const generateManager = manager => {
         return `
-        <div class="card employee-card">
-        <div class="card-header">
+        <div class="card employee-card m-3">
+        <div class="card-header bg-primary">
             <h2 class="card-title">${manager.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
@@ -23,8 +23,8 @@ const generateTeam = team => {
     // create the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
+        <div class="card employee-card m-3">
+    <div class="card-header bg-warning">
         <h2 class="card-title">${engineer.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
     </div>
@@ -42,8 +42,8 @@ const generateTeam = team => {
     // create the html for interns
     const generateIntern = intern => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
+        <div class="card employee-card m-3">
+    <div class="card-header bg-warning">
         <h2 class="card-title">${intern.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
     </div>
@@ -80,7 +80,7 @@ const generateTeam = team => {
 }
 
 // export function to generate entire page
-module.exports = team => {
+module.exports = function(team) {
 
     return `
     <!DOCTYPE html>
@@ -100,7 +100,7 @@ module.exports = team => {
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
+            <div class="col-12 jumbotron mb-3 team-heading bg-success">
                 <h1 class="text-center">My Team</h1>
             </div>
         </div>
